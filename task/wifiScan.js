e18e7d97d0ca3a4e692d1e74d083d641915bd07e
xoxulin <alex.hohulin@gmail.com>
2012-12-14 16:47:14 +0400

    wifi parameters lowercase

--- task/wifiScan.js
+++ task/wifiScan.js
@@ -51,11 +51,11 @@ util.extend (wifiScanTask.prototype, {
 			currentWiFi,
 			lastParameter
 			cells = output.split (/Cell \d+ - /),
-			parameterRe = /^\s*([^:=])(:|=)\s*(.*?)\s*$/,
+			parameterRe = /^\s*([^:=]+)(:|=)\s*(.*?)\s*$/,
 			continueRe = /^\s*(.*?)\s*$/;
 		
 		cells.shift(); //remove 'wifi scan completed'
-	
+		console.log('CELLS:', cells.length)
 		cells.forEach(function (cell) {
 		
 			currentWiFi = {};
@@ -65,8 +65,9 @@ util.extend (wifiScanTask.prototype, {
 			
 			parameters.forEach(function(parameter) {
 				var match = parameter.match(parameterRe);
+				
 				if (match) {
-					lastParameter = match[1];
+					lastParameter = match[1].toLowerCase().replace(/\s+/g,'_');
 					currentWiFi[lastParameter] = match[3];
 				} else {
 					var spaceMatch = parameter.match(continueRe);
