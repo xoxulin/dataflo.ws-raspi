e16fae3503bede54e269513cc831933407254f82
xoxulin <alex.hohulin@gmail.com>
2013-02-08 21:02:10 +0400

    wmr driver/initiator completed

--- initiator/wmr.js
+++ initiator/wmr.js
@@ -91,16 +91,16 @@ var VID_OREGON = 0x0fde,
 	UVI_DATA 					= 0xD5,
 	PRESSURE_DATA 				= 0xD6,
 	TEMPERATURE_HUMIDITY_DATA 	= 0xD7,
-	BATERY_STATUS_DATA			= 0xD9,
+	BATERY_STATUS_DATA			= 0xD9;
 
-	LENGTH = {
-		0xD2: 49,
-		0xD3: 16,
-		0xD4: 22,
-		0xD5: 10,
-		0xD6: 13,
-		0xD7: 16,
-		0xD9: 8
+	ALLOWED_HEADERS = {
+		0xD2: true,
+		0xD3: true,
+		0xD4: true,
+		0xD5: true,
+		0xD6: true,
+		0xD7: true,
+		0xD9: true
 	};
 
 // - - -
@@ -136,6 +136,10 @@ util.extend (wmri.prototype, {
 		var self = this;
 		
 		console.log('WMR daemon started');
+		
+		self.currentHeader = null;
+		self.currentBuffer = null;
+		self.currentBufferPosition = 0;
 		
 		self.hid.write(RESET_CMD);
 		self.hid.write(HISTORY_CMD);
@@ -203,42 +207,64 @@ util.extend (wmri.prototype, {
 		
 		var self = this;
 		
-		if (self.currentHeader && self.currentBuffer) {
+		if (self.currentHeader === null && !self.currentBuffer) {
+			
+			// if header not allowed then ignore
+			
+			if (!ALLOWED_HEADERS[byte]) return;
+			
+			// if no header no buffer
+			
+			self.currentHeader = byte;
+			
+		} else if (self.currentHeader !== null && !self.currentBuffer) {
+		
+			// if header no buffer
+			
+			self.currentBuffer = new Buffer(byte);
+			self.currentBufferPosition = 0;
+			self.currentBuffer.writeUInt8(self.currentHeader, self.currentBufferPosition++);
+			self.currentBuffer.writeUInt8(byte, self.currentBufferPosition++);
+			
+		} else if (self.currentHeader !== null && self.currentBuffer) {
 			
 			self.currentBuffer.writeUInt8(byte, self.currentBufferPosition++);
 			
 			if (self.currentBufferPosition == self.currentBuffer.length) {
 				
-				var checkSumValid = true;
+				var data = true;
 				
 				switch (self.currentHeader) {
 					
 					case HISTORIC_DATA:
-						checkSumValid = self.processHistory(self.currentBuffer);
+						data = self.processHistory(self.currentBuffer);
 					break;
 					case WIND_DATA:
-						checkSumValid = self.processWind(self.currentBuffer);
+						data = self.processWind(self.currentBuffer);
 					break;
 					case RAIN_DATA:
-						checkSumValid = self.processRain(self.currentBuffer);
+						data = self.processRain(self.currentBuffer);
 					break;
 					case UVI_DATA:
-						checkSumValid = self.processUVI(self.currentBuffer);
+						data = self.processUVI(self.currentBuffer);
 					break;
 					case PRESSURE_DATA:
-						checkSumValid = self.processPressure(self.currentBuffer);
+						data = self.processPressure(self.currentBuffer);
 					break;
 					case TEMPERATURE_HUMIDITY_DATA:
-						checkSumValid = self.processTemperatureHumidity(self.currentBuffer);
+						data = self.processTemperatureHumidity(self.currentBuffer);
 					break;
 					case BATERY_STATUS_DATA:
-						checkSumValid = self.processBateryStatus(self.currentBuffer);
+						data = self.processBatteryStatus(self.currentBuffer);
 					break;
 					
 				}
 				
-				if (!checkSumValid) {
+				if (data === false) {
+					console.log(self.currentHeader, data);
 					self.reset();
+				} else {
+					self.getAndStartWorkflow(data);
 				}
 				
 				self.currentHeader = null;
@@ -247,54 +273,408 @@ util.extend (wmri.prototype, {
 				
 			}
 			
-		} else if (LENGTH[byte]) {
-		
-			self.currentHeader = byte;
-			self.currentBuffer = new Buffer(LENGTH[byte]);
-			self.currentBufferPosition = 0;
-			self.currentBuffer.writeUInt8(byte, self.currentBufferPosition++);
-		
-		} else {
-			
-			self.currentHeader = null;
-			self.currentBuffer = null;
-			self.currentBufferPosition = 0;
-			
 		}
 	},
 	
 	processHistory: function(buffer) {
-		console.log('history', buffer);
-		return true;
+	
+		if (!this.getCheckSumValid(buffer)) return false;
+		
+			// - - - time & date
+			
+		var timestamp = this.getTimestamp(buffer),
+		
+			// - - - rain
+			
+			rain = this._processRain(buffer, 7),
+			
+			// - - - wind
+
+			wind = this._processWind(buffer, 20);
+			
+			// - - - uvi
+
+			uvi = this._processUVI(buffer, 27),
+
+			// - - - baro
+
+			pressure = this._processPressure(buffer, 28),
+
+			// - - - external sensor count
+
+			externalSensorCount = buffer.readUInt8(32),
+
+			// - - - indoor sensors
+
+			indoorTemperatureHumidity = this._processTemperatureHumidity(buffer, 33),
+			
+			data = [rain, wind, uvi, pressure];
+			
+			data.push.apply(data, indoorTemperatureHumidity);
+
+			rain. timestamp = wind.timestamp =
+			uvi.timestamp = pressure.timestamp =
+			indoorTemperatureHumidity[0].timestamp =
+			indoorTemperatureHumidity[1].timestamp = timestamp;
+			
+			
+			
+			// - - - external sensors
+			
+			for (var i = 0; i < externalSensorCount; i++) {
+			
+				var externalSensors = this._processTemperatureHumidity(buffer, 40 + 7*i);
+
+				externalSensors[0].timestamp =
+				externalSensors[1].timestamp = timestamp;
+
+				data.push.apply(data, indoorTemperatureHumidity);
+			
+			}
+		
+		return data;
 	},
 	
 	processWind: function(buffer) {
-		console.log('wind', buffer);
-		return true;
+		
+		if (!this.getCheckSumValid(buffer)) return false;
+		
+			// - - - time & date
+			
+		var timestamp = this.getTimestamp(buffer),
+		
+			// - - - wind
+
+			data = this._processWind(buffer);
+			
+		data.timestamp = timestamp;
+		
+		return data;
+	},
+	
+	_processWind: function(buffer, offset) {
+		
+		if (!offset) offset = 7
+		
+		var D = buffer.readUInt8(offset),
+			windDirection = D & 0x0F,
+
+			gg = buffer.readUInt8(1+offset),
+			aG = buffer.readUInt8(2+offset),
+			aA = buffer.readUInt8(3+offset),
+			windGust = (((aG & 0x0F) * 256) + gg) / 10,
+			windAvg = (aA * 16 + (aG >> 4)) / 10,
+
+			WC = buffer.readUInt8(4+offset),
+			windChill = (WC - 32) / 1.8;
+			
+		return {
+			type: 'wind',
+			direction: windDirection,
+			gust: windGust,
+			avg: windAvg,
+			chill: windChill
+		};
+		
 	},
 	
 	processRain: function(buffer) {
-		console.log('rain', buffer);
-		return true;
+		
+		if (!this.getCheckSumValid(buffer)) return false;
+		
+			// - - - time & date
+			
+		var timestamp = this.getTimestamp(buffer),
+		
+			// - - - rain
+
+			data = this._processRain(buffer);
+			
+		data.timestamp = timestamp;
+		
+		return data;
+	},
+	
+	_processRain: function(buffer, offset) {
+		
+		if (!offset) offset = 7;
+		
+		var rL = buffer.readUInt8(offset),
+			rH = buffer.readUInt8(1+offset),
+			
+			rainRate = (((rH*256) + rL) / 100) * 25.4,
+		
+			hL = buffer.readUInt8(2+offset),
+			hH = buffer.readUInt8(3+offset),
+
+            rainHour = (((hH*256) + hL) / 100) * 25.4,
+
+			dL = buffer.readUInt8(4+offset),
+			dH = buffer.readUInt8(5+offset),
+
+   			rain24Hour = (((dH*256) + dL) / 100) * 25.4,
+
+			aL = buffer.readUInt8(6+offset),
+			aH = buffer.readUInt8(7+offset),
+
+			rainAccum = (((aH*256) + aL) / 100) * 25.4,
+
+			accumTimestamp = this.getTimestamp(buffer, 8+offset);
+			
+		return {
+			type: 'rain',
+			rate : rainRate,
+			rateHour : rainHour,
+			rate24Hour: rain24Hour,
+			accum: rainAccum,
+			accumDate: accumTimestamp
+		};
+		
 	},
 	
 	processUVI: function(buffer) {
-		console.log('uvi', buffer);
-		return true;
+		
+		if (!this.getCheckSumValid(buffer)) return false;
+		
+			// - - - time & date
+			
+		var timestamp = this.getTimestamp(buffer),
+		
+			// - - - uvi
+			
+			UV = buffer.readUInt8(7),
+			
+			uvi = UV & 0x0F,
+			
+			data = {
+				type: 'uvi',
+				timestamp: timestamp,
+				value: uvi
+			};
+							
+		return data;
+	},
+	
+	_processUVI: function(buffer, offset) {
+	
+		if (!offset) offset = 7
+		
+		var UV = buffer.readUInt8(offset);
+			
+		return {
+			type: 'uvi',
+			value: UV & 0x0F
+		};
+		
 	},
 	
 	processPressure: function(buffer) {
-		console.log('pressure', buffer);
-		return true;
+		
+		if (!this.getCheckSumValid(buffer)) return false;
+		
+			// - - - time & date
+			
+		var timestamp = this.getTimestamp(buffer),
+		
+			// - - - data
+			
+			data = this._processPressure(buffer);
+			
+			data.timestamp = timestamp;
+		
+		return data;
+	},
+
+	_processPressure: function(buffer, offset) {
+		
+		if (!offset) offset = 7;
+		
+			// - - - pressure
+			
+		var	SS = buffer.readUInt8(offset),
+			iS = buffer.readUInt8(1+offset),
+			
+			pressure = ((iS & 0x0F) * 256 + SS) * 0.7500616827042,
+			
+			// - - - forecast
+			
+			forecast = iS >> 4,
+			
+			// - - - altitude
+			
+			AA = buffer.readUInt8(2+offset),
+			xA = buffer.readUInt8(3+offset),
+			
+			altitude = ((xA & 0x0F) * 256 + AA) * 7.88801728927;
+			
+		return {
+			type: 'pressure',
+			value: pressure,
+			forecast: forecast,
+			altitude: altitude
+		};
+		
 	},
 	
 	processTemperatureHumidity: function(buffer) {
-		console.log('temperature_humidity', buffer);
-		return true;
+	
+		if (!this.getCheckSumValid(buffer)) return false;
+		
+			// - - - time & date
+			
+		var timestamp = this.getTimestamp(buffer),
+			
+			// - - - sensors & trends
+			
+			data = this._processTemperatureHumidity(buffer);
+			
+		data[0].timestamp = timestamp;
+		data[1].timestamp = timestamp;
+		
+		return data;
+	},
+
+	_processTemperatureHumidity: function(buffer, offset) {
+		
+		if (!offset) offset = 7;
+		
+			// - - - sensors & trends
+			
+		var temHumSensor = buffer.readUInt8(offset),
+			temperatureTrend = temHumSensor >> 6,
+			humidityTrend = temHumSensor >> 4 & 0x03,
+			sensorNum = temHumSensor & 0x0F,
+			
+			// - - - temperature
+			
+			TT = buffer.readUInt8(1+offset),
+			sT = buffer.readUInt8(2+offset),
+			temperatureSign = ((sT >> 7) == 1) ? -1 : 1,
+			temperature = temperatureSign * ((sT & 0x7F) * 256 + TT) / 10,
+			
+			// - - - humidity
+			
+			humidity = buffer.readUInt8(3+offset),
+			
+			// - - - dew point
+			
+			DD = buffer.readUInt8(4+offset),
+			sD = buffer.readUInt8(5+offset),
+			dewSign = ((sT >> 7) == 1) ? -1 : 1,
+			dewTemperature = dewSign * ((sD & 0x7F) * 256 + DD) / 10,
+			
+			// - - - heat
+			
+			heatIndex = (buffer.readUInt8(6+offset) - 32) / 1.8;
+			
+		return [{
+			type: 'temperature',
+			sensorNum: sensorNum,
+			value: temperature,
+			trend: temperatureTrend,
+			heatIndex: heatIndex
+		}, {
+			type: 'humidity',
+			sensorNum: sensorNum,
+			value: humidity,
+			trend: humidityTrend,
+			dewPoint: dewTemperature
+		}];
+		
 	},
 	
-	processBateryStatus: function(buffer) {
-		console.log('batery_status', buffer);
-		return true;
+	processBatteryStatus: function(buffer) {
+	
+		if (!this.getCheckSumValid(buffer)) return false;
+		
+			// - - - fault & battery
+			
+		var SW = buffer.readUInt8(2),
+			tempHumSensorFault = (SW & 0x02) == 0x02,
+			windSensorFault = (SW & 0x01) == 0x01,
+			
+   			UR = buffer.readUInt8(3),
+			uviSensorFault = (SW & 0x80) == 0x80,
+			rainSensorFault = (SW & 0x40) == 0x40,
+			
+			RB = buffer.readUInt8(3),
+			rfSignalWeak = (RB & 0x80) == 0x80,
+			tempHumBatteryLow = (RB & 0x02) == 0x02,
+			windBatteryLow = (RB & 0x01) == 0x01,
+			
+			BB = buffer.readUInt8(3),
+			uviBatteryLow = (BB & 0x20) == 0x20,
+			rainBatteryLow = (RB & 0x10) == 0x10,
+
+
+   			data = {
+				type: 'status',
+				sensorFault: {
+					tempHum: tempHumSensorFault,
+					wind: windSensorFault,
+					uvi: uviSensorFault,
+					rain: rainSensorFault
+				},
+				batteryLow: {
+					tempHum: tempHumBatteryLow,
+					wind: windBatteryLow,
+					uvi: uviBatteryLow,
+					rain: rainBatteryLow
+				},
+				signalWeak: rfSignalWeak
+			};
+		
+		return data;
+	},
+	
+	getTimestamp: function(buffer, offset) {
+		
+		if (!offset) offset = 2;
+		
+		var date = new Date(
+				buffer.readUInt8(4+offset) + 2000 ,
+				buffer.readUInt8(3+offset),
+				buffer.readUInt8(2+offset),
+				buffer.readUInt8(1+offset),
+				buffer.readUInt8(offset)
+			);
+		
+		return ~~(date.getTime()/1000);
+		
+	},
+	
+	getCheckSumValid: function(buffer) {
+		
+		var length = buffer.readUInt8(1);
+			
+		if (buffer.length != length) return false;
+			
+		var	cL = buffer.readUInt8(length-2),
+			cH = buffer.readUInt8(length-1),
+			checkSum = 0;
+		
+		Array.prototype.every.call(buffer, function(byte, ix) {
+			
+			if (ix < length - 2) {
+				checkSum += byte;
+				return true;
+			}
+			
+			return false;
+			
+		});
+		
+		return (checkSum == 256*cH + cL);
+		
+	},
+	
+	getAndStartWorkflow: function(data) {
+		
+		if (data.constructor !== Array) data = [data];
+		
+		data.forEach(function(item) {
+			if (item) console.log('<<<', item.type, item);
+		});
+		
 	}
 });
