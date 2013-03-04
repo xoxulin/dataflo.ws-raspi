de883e8ba45ac2b7ba044804fd29272b9672ed79
xoxulin <alex.hohulin@gmail.com>
2013-03-04 14:03:14 +0400

    remove state from task

--- task/state.js
+++ task/state.js
@@ -1,8 +1,7 @@
 var EventEmitter = require ('events').EventEmitter,
 	task         = require ('dataflo.ws/task/base'),
 	util         = require ('util'),
-	os	 	 	 = require('os'),
-	io        	 = require('dataflo.ws/io/easy');
+	os	 	 	 = require('os');
 
 var stateTask = module.exports = function (config) {
 
@@ -31,65 +30,10 @@ stateTask.prototype.run = function () {
 				networkInterfaces: os.networkInterfaces(),
 				uptime: process.uptime()
 				
-			}
+			},
+			cpuInfo: project.cpuInfo
 		};
 		
-	self.getCPUInfo(function(cpuInfo) {
-		
-		state.cpuInfo = cpuInfo;
-		self.completed(state);
-		
-	});
+	self.completed(state);
 	
-}
-
-stateTask.prototype.getCPUInfo = function(callback) {
-
-	if (project.cpuInfo) {
-		
-		callback(project.cpuInfo);
-		
-		return;
-		
-	}
-	
-	var self = this,
-		cpuInfo = new io('/proc/cpuinfo');
-		
-	cpuInfo.readFile(function(error, data) {
-		
-		if (error) {
-			self.failed(error);
-			return;
-		}
-		
-		project.cpuInfo = self.parseCPUInfoList(data);
-		callback(project.cpuInfo);
-		
-	});
-
-}
-
-stateTask.prototype.parseCPUInfoList = function(data) {
-
-	data = data.toString().split('\n');
-	
-	var pair, key, value,
-		result = {};
-	
-	data.forEach(function(line) {
-		
-		if (line && line.replace(/\s+?/).length) {
-			
-			pair = line.split(/\t*?: /);
-			key = pair[0].replace(/\s+/,'_').toLowerCase();
-			value = pair[1].replace(/\s+$/,'');
-			
-			result[key] = value;
-		}
-		
-	});
-	
-	return result;
-
 }
